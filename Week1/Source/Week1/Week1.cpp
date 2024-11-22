29bef625b961ec965b94398ab2362214b3602295
Li Jianping <1196934490@qq.com>
2024-11-22 11:35:41 +0800

    Package week 1

---
+++ Week1/Source/Week1/Week1.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Week1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Week1, "Week1" );
+ 
---
+++ Week1/Source/Week1/Week1GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Week1GameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWeek1GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWeek1GameMode();
+};
+
+
+
